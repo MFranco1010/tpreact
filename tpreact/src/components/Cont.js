c101a0427c4e5c9d0afa5bf9a6ec36e095578d06
gastoon16 <88505051+MFranco1010@users.noreply.github.com>
2023-08-23 14:03:22 -0300

    nav terminado

--- tpreact/src/components/Cont.js
+++ tpreact/src/components/Cont.js
@@ -3,7 +3,9 @@ import React from 'react'
 export const Cont = () => {
     return (
         <div className='cont'>
-            
+            <div className='Card'>
+                <img  />
+            </div>
         </div>
     )
 }
