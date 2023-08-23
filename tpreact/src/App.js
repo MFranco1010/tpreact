5f4e948bd6f2171afda4ebecd0e2d85315fdf21a
gastoon16 <88505051+MFranco1010@users.noreply.github.com>
2023-08-23 14:02:15 -0300

    nav terminado

--- tpreact/src/App.js
+++ tpreact/src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import { Nav } from './components/Nav';
+import { Foot } from './components/Foot';
+import { Cont } from './components/Cont';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <Nav />
       </header>
+      <div>
+        <Cont />
+      </div>
     </div>
   );
 }
--- tpreact/src/components/Nav.js
+++ tpreact/src/components/Nav.js
@@ -3,7 +3,12 @@ import React from 'react'
 export const Nav = () => {
     return (
         <div className='nav'>
-            nav
+            <a className='logo' href="/">JOJO</a>
+            <ul className='about'>
+                <li><a className='abo' href="/">About</a></li>
+                <li><a className='abo' href="/">More</a></li>
+                <li><a className='abo' href="/">Dio</a></li>
+            </ul>
         </div>
     )
 }
