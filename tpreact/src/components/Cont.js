54827671e4f8a1dea1ee0ff36a22117c40331673
Frantass <88505051+MFranco1010@users.noreply.github.com>
2023-08-29 02:17:47 -0300

    mejoras esteticas

--- tpreact/src/components/Cont.js
+++ tpreact/src/components/Cont.js
@@ -4,24 +4,15 @@ export const Cont = () => {
     return (
         <div className='cont'>
             <div className='card'>
-                <img className='img' src="https://picsum.photos/350/200" alt=""/>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id expedita iure perspiciatis, odio architecto, quia nam porro fugiat obcaecati qui voluptatibus adipisci, incidunt ut! Asperiores sunt impedit eligendi nulla doloribus?</p>
-            </div>
-            <div className='card'>
-                <img className='img' src="https://picsum.photos/350/200" alt=""/>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id expedita iure perspiciatis, odio architecto, quia nam porro fugiat obcaecati qui voluptatibus adipisci, incidunt ut! Asperiores sunt impedit eligendi nulla doloribus?</p>
-            </div>
-            <div className='card'>
-                <img className='img' src="https://picsum.photos/350/200" alt=""/>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id expedita iure perspiciatis, odio architecto, quia nam porro fugiat obcaecati qui voluptatibus adipisci, incidunt ut! Asperiores sunt impedit eligendi nulla doloribus?</p>
-            </div>
-            <div className='card'>
-                <img className='img' src="https://picsum.photos/350/200" alt=""/>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id expedita iure perspiciatis, odio architecto, quia nam porro fugiat obcaecati qui voluptatibus adipisci, incidunt ut! Asperiores sunt impedit eligendi nulla doloribus?</p>
-            </div>
-            <div className='card'>
-                <img className='img' src="https://picsum.photos/350/200" alt=""/>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id expedita iure perspiciatis, odio architecto, quia nam porro fugiat obcaecati qui voluptatibus adipisci, incidunt ut! Asperiores sunt impedit eligendi nulla doloribus?</p>
+                <img className='img' src="https://picsum.photos/350/240" alt=""/>
+                <h1>PASTEL DE PURA MANTECA</h1>
+                <p>Comienza batiendo la manteca hasta obtener una textura cremosa. Agrega el azúcar y los huevos, uno por uno, asegurándote de que se mezclen bien. Integra la esencia de vainilla y una pizca de sal. Suavemente, añade la harina tamizada hasta lograr una mezcla homogénea. Vierte en un molde engrasado y hornea a 180°C durante 45-50 minutos. Deja enfriar antes de desmoldar y disfruta.</p>
+                <ul className='lista'>
+                    <li className='li reloj'>45'-50'</li>
+                    <li className='li sal'> al gusto </li>
+                    <li className='li manteca'> mucha (1266g)</li>
+                    <li className='li harina'> 3.22g </li>
+                </ul>
             </div>
         </div>
     )
