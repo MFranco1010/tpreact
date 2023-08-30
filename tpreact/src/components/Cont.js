7280f89e732b6f427ece22770293c038cbf5975e
Frantass <88505051+MFranco1010@users.noreply.github.com>
2023-08-30 01:23:48 -0300

    responsivo

--- tpreact/src/components/Cont.js
+++ tpreact/src/components/Cont.js
@@ -6,7 +6,7 @@ export const Cont = () => {
     return (
         <div className='cont'>
             <Card
-                imageSrc="https://picsum.photos/350/240"
+                imageSrc="https://picsum.photos/650/440"
                 title="Panqueques"
                 description="Mezcla 1 taza de harina, 2 huevos, pizca de sal y 1 cucharada de azúcar en un tazón. Agrega 1 ½ tazas de leche gradualmente. Calienta sartén y engrásala con manteca. Vierte la mezcla en sartén caliente, cocina hasta dorar bordes, luego voltea y cocina otro lado. Repite con el resto de la mezcla. Sirve con miel, jarabe de arce u otros toppings preferidos."
                 time="20'-30'"
@@ -16,7 +16,7 @@ export const Cont = () => {
                 egg="Dos huevos."
             />
             <Card
-                imageSrc="https://picsum.photos/350/240"
+                imageSrc="https://picsum.photos/650/440"
                 title="Galletas"
                 description="Calienta horno a 180°C (350°F). Usa papel pergamino en bandeja. Bate manteca y azúcar hasta suavizar. Agrega huevo y mezcla bien. Combina harina y sal en otro tazón. Añade mezcla de harina a manteca. Forma bolitas en bandeja. Aplana con tenedor. Hornea 10-12 minutos, bordes dorados. Enfría en bandeja antes de pasar a rejilla."
                 time="15-30'"
@@ -26,7 +26,7 @@ export const Cont = () => {
                 egg="Un huevo"
             />
             <Card
-                imageSrc="https://picsum.photos/350/240"
+                imageSrc="https://picsum.photos/650/440"
                 title="Muffins de Arándanos"
                 description="Precalienta el horno a 200°C (400°F). Coloca papel para muffins en el molde. Combina harina, azúcar, polvo de hornear y sal. Bate huevo, leche y manteca derretida. Agrega lo líquido a la harina, revuelve levemente. Añade arándanos y mezcla. Llena cavidades del molde 2/3. Hornea 18-20 minutos. Enfría en molde antes de transferir a rejilla."
                 time="40'-60'"
@@ -36,7 +36,7 @@ export const Cont = () => {
                 egg="Un huevo"
             />
             <Card
-                imageSrc="https://picsum.photos/350/240"
+                imageSrc="https://picsum.photos/650/440"
                 title="Scones de Arándanos"
                 description="Calienta horno a 180°C (350°F). Usa papel pergamino en bandeja. Bate manteca y azúcar hasta suavizar. Agrega huevo y mezcla. Combina harina y sal en otro tazón. Agrega mezcla de harina a manteca. Forma bolitas en bandeja. Aplana con tenedor. Hornea 10-12 minutos, bordes dorados. Enfría en bandeja antes de pasar a rejilla."
                 time="40'-60'"
@@ -46,7 +46,7 @@ export const Cont = () => {
                 egg="Un huevo"
             />
             <Card
-                imageSrc="https://picsum.photos/350/240"
+                imageSrc="https://picsum.photos/650/440"
                 title="PASTEL DE PURA MANTECA"
                 description="Comienza batiendo la manteca hasta obtener una textura cremosa. Agrega el azúcar y los huevos, uno por uno, asegurándote de que se mezclen bien. Integra la esencia de vainilla y una pizca de sal. Suavemente, añade la harina tamizada hasta lograr una mezcla homogénea. Vierte en un molde engrasado y hornea a 180°C durante 45-50 minutos. Deja enfriar antes de desmoldar y disfruta."
                 time="45'-50'"
